e14e8dd852517798f02957bbdac2bcf2aca959eb
도훈 <modooboiroo@gmail.com>
2024-11-01 21:25:24 +0900

    feat :: Board

---
+++ src/main/kotlin/org/example/firstkopring/domain/board/domain/Board.kt
@@ -0,0 +1,20 @@
+package org.example.firstkopring.domain.board.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class Board(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val Id: Long?,
+
+    @Column(name = "title", nullable = false, unique = true)
+    val title: String,
+
+    @Column(name = "content", nullable = true)
+    val content: String
+)
