230ec42c597a2aa4d4acc0b62668114bae634258
도훈 <modooboiroo@gmail.com>
2024-11-01 20:29:28 +0900

    init project

---
+++ src/main/kotlin/org/example/firstkopring/FirstKopringApplication.kt
@@ -0,0 +1,11 @@
+package org.example.firstkopring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstKopringApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstKopringApplication>(*args)
+}
