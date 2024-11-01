bdb0b9bb4645cf666c5e28af132dc3fd037f964e
도훈 <modooboiroo@gmail.com>
2024-11-01 21:37:24 +0900

    feat :: addBoardService

---
+++ src/main/kotlin/org/example/firstkopring/domain/board/sevice/addBoardService.kt
@@ -0,0 +1,22 @@
+package org.example.firstkopring.domain.board.sevice
+
+import org.example.firstkopring.domain.board.domain.Board
+import org.example.firstkopring.domain.board.domain.presentation.dto.request.addBoardRequest
+import org.example.firstkopring.domain.board.domain.repository.BoardRepository
+import org.springframework.stereotype.Service
+
+@Service
+class addBoardService(
+    private val boardRepository: BoardRepository
+) {
+    fun execute(addBoardRequest: addBoardRequest) {
+        boardRepository.save(
+            addBoardRequest.run {
+                Board(
+                    title = title,
+                    content = content
+                )
+            }
+        )
+    }
+}
