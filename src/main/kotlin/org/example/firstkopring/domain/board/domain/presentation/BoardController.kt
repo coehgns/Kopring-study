69b63225a948e9dc7dcde05c9580ab837ce1a075
도훈 <modooboiroo@gmail.com>
2024-11-01 21:45:48 +0900

    feat :: BoardController

---
+++ src/main/kotlin/org/example/firstkopring/domain/board/domain/presentation/BoardController.kt
@@ -0,0 +1,38 @@
+package org.example.firstkopring.domain.board.domain.presentation
+
+import org.example.firstkopring.domain.board.domain.Board
+import org.example.firstkopring.domain.board.domain.presentation.dto.request.addBoardRequest
+import org.example.firstkopring.domain.board.sevice.DeleteBoardService
+import org.example.firstkopring.domain.board.sevice.GetAllBoardService
+import org.example.firstkopring.domain.board.sevice.GetBoardService
+import org.example.firstkopring.domain.board.sevice.addBoardService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/boards")
+class BoardController(
+    private val addBoardService: addBoardService,
+    private val getBoardService: GetBoardService,
+    private val getAllBoardService: GetAllBoardService,
+    private val deleteBoardService: DeleteBoardService
+) {
+    @PostMapping
+    fun addBoard(@RequestBody addBoardRequest: addBoardRequest) {
+        addBoardService.execute(addBoardRequest)
+    }
+
+    @GetMapping("/{id}")
+    fun getBoard(@PathVariable id: Long) : Board = getBoardService.execute(id)
+
+    @GetMapping
+    fun getAllBoard() : List<Board> = getAllBoardService.execute()
+
+    @DeleteMapping("/{id}")
+    fun deleteBoard(@PathVariable id: Long) = deleteBoardService.execute(id)
+}
