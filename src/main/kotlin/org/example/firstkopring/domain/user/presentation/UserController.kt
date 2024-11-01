c3efb4cf8e71319f2579f0b572c6b8e89d0b00ea
도훈 <modooboiroo@gmail.com>
2024-11-01 21:54:28 +0900

    refactor :: UserController 로직 리팩토링

--- src/main/kotlin/org/example/firstkopring/domain/user/presentation/UserController.kt
+++ src/main/kotlin/org/example/firstkopring/domain/user/presentation/UserController.kt
@@ -2,8 +2,10 @@ package org.example.firstkopring.domain.user.presentation
 
 import org.example.firstkopring.domain.user.domain.User
 import org.example.firstkopring.domain.user.presentation.dto.request.SaveUserRequest
-import org.example.firstkopring.domain.user.presentation.dto.response.GetUserResponse
-import org.example.firstkopring.domain.user.service.UserService
+import org.example.firstkopring.domain.user.service.DeleteUserService
+import org.example.firstkopring.domain.user.service.GetAllUserService
+import org.example.firstkopring.domain.user.service.GetUserService
+import org.example.firstkopring.domain.user.service.SaveUserService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,17 +17,20 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/users")
 class UserController(
-    private val userService: UserService
+    private val saveUserService: SaveUserService,
+    private val getUserService: GetUserService,
+    private val getAllUserService: GetAllUserService,
+    private val deleteUserService: DeleteUserService
 ) {
     @PostMapping
-    fun addUser(@RequestBody saveUserRequest: SaveUserRequest) = userService.saveUser(saveUserRequest)
+    fun saveUser(@RequestBody saveUserRequest: SaveUserRequest) = saveUserService.execute(saveUserRequest)
 
     @GetMapping("/{id}")
-    fun findUser(@PathVariable id: Long) : User = userService.findUser(id)
+    fun getUser(@PathVariable id: Long) : User = getUserService.execute(id)
 
     @GetMapping
-    fun selectAllUser() : List<User> = userService.selectAllUser()
+    fun getAllUser() : List<User> = getAllUserService.execute()
 
     @DeleteMapping("/{id}")
-    fun deleteUser(@PathVariable id: Long) = userService.deleteUser(id)
+    fun deleteUser(@PathVariable id: Long) = deleteUserService.execute(id)
 }
