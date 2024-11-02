129cfdb1b24127621ae914f4c024727093afa9b8
도훈 <modooboiroo@gmail.com>
2024-11-02 20:16:28 +0900

    feat :: ModifyBoardService

---
+++ src/main/kotlin/org/example/firstkopring/domain/board/sevice/ModifyBoardService.kt
@@ -0,0 +1,18 @@
+package org.example.firstkopring.domain.board.sevice
+
+import org.example.firstkopring.domain.board.domain.repository.BoardRepository
+import org.example.firstkopring.domain.board.presentation.dto.request.ModifyBoardRequest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class ModifyBoardService(
+    private val boardRepository: BoardRepository
+) {
+    fun execute(id: Long, modifyBoardRequest: ModifyBoardRequest) {
+        val board = boardRepository.findByIdOrNull(id)
+            ?: throw RuntimeException("board not exception")
+
+        board.modifyBoard(modifyBoardRequest.title, modifyBoardRequest.content)
+    }
+}
