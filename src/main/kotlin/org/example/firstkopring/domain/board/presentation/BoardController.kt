6f1d4cbda2e85c63ca586fa2bb081a12b4f40b7b
도훈 <modooboiroo@gmail.com>
2024-11-03 20:18:11 +0900

    refactor :: HTTP 메서드 수정

--- src/main/kotlin/org/example/firstkopring/domain/board/presentation/BoardController.kt
+++ src/main/kotlin/org/example/firstkopring/domain/board/presentation/BoardController.kt
@@ -6,6 +6,7 @@ import org.example.firstkopring.domain.board.presentation.dto.request.ModifyBoar
 import org.example.firstkopring.domain.board.sevice.*
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -36,7 +37,7 @@ class BoardController(
     @DeleteMapping("/{id}")
     fun deleteBoard(@PathVariable id: Long) = deleteBoardService.execute(id)
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     fun modifyBoard(@PathVariable id: Long, @RequestBody modifyBoardRequest: ModifyBoardRequest) {
         modifyBoardService.execute(id, modifyBoardRequest)
     }
