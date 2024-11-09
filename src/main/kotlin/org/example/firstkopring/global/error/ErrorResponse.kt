12dff70206e3379cc150eea950c2583f838ad0eb
도훈 <modooboiroo@gmail.com>
2024-11-09 23:43:53 +0900

    feat :: 정적 팩토리 메서드

--- src/main/kotlin/org/example/firstkopring/global/error/ErrorResponse.kt
+++ src/main/kotlin/org/example/firstkopring/global/error/ErrorResponse.kt
@@ -1,8 +1,21 @@
 package org.example.firstkopring.global.error
 
+import org.example.firstkopring.global.error.exception.ErrorCode
+
 
 data class ErrorResponse(
     val message: String,
     val statusCode: Int,
     val description: String,
-)
+) {
+
+    companion object{
+        fun of(errorCode: ErrorCode, message: String): ErrorResponse {
+            return ErrorResponse(
+                statusCode = errorCode.statusCode,
+                message = errorCode.message,
+                description = errorCode.message,
+            )
+        }
+    }
+}
