e2757d5232a7ab8bfab75273f27ec6e9111a81c1
도훈 <modooboiroo@gmail.com>
2024-11-09 23:49:20 +0900

    refactor :: 불필요한 import문 제거

--- src/main/kotlin/org/example/firstkopring/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/example/firstkopring/global/error/GlobalExceptionHandler.kt
@@ -3,7 +3,6 @@ package org.example.firstkopring.global.error
 import org.example.firstkopring.global.error.exception.BusinessException
 import org.example.firstkopring.global.error.exception.ErrorCode
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
