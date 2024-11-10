3913bd27d7fda972235118bb833e06ecc78c2a09
도훈 <modooboiroo@gmail.com>
2024-11-10 21:54:18 +0900

    refactor :: 예외 수정

--- src/main/kotlin/org/example/firstkopring/domain/user/service/GetUserService.kt
+++ src/main/kotlin/org/example/firstkopring/domain/user/service/GetUserService.kt
@@ -2,6 +2,8 @@ package org.example.firstkopring.domain.user.service
 
 import org.example.firstkopring.domain.user.domain.User
 import org.example.firstkopring.domain.user.domain.repository.UserRepository
+import org.example.firstkopring.global.error.exception.BusinessException
+import org.example.firstkopring.global.error.exception.ErrorCode
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
@@ -11,5 +13,5 @@ class GetUserService(
 ) {
     fun execute(id: Long) : User =
         userRepository.findByIdOrNull(id)
-            ?: throw RuntimeException("user not exception")
+            ?: throw BusinessException(ErrorCode.USER_NOT_FOUND)
 }
