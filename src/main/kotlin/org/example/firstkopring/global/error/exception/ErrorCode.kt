f36c5a4a5a18c91289cdede5b1021ac18c051893
도훈 <modooboiroo@gmail.com>
2024-11-12 08:29:57 +0900

    feat :: add PASSWORD_MISMATCH Exception to ErrorCode

--- src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
@@ -5,6 +5,7 @@ enum class ErrorCode(
     val message: String,
 ) {
     INVALID_TOKEN(401, "invalid token"),
+    PASSWORD_MISMATCH(401, "password mismatch"),
     BOARD_NOT_FOUND(404, "board not found."),
     USER_NOT_FOUND(404, "user not found"),
     USER_ALREADY_EXISTS(409, "User Already Exists"),
