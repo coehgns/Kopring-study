828212dd2190d03d54aa2e1009785b836c9ebefa
도훈 <modooboiroo@gmail.com>
2024-11-11 09:11:54 +0900

    feat :: RefreshToken

---
+++ src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
@@ -0,0 +1,12 @@
+package org.example.firstkopring.domain.auth.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+
+@Entity
+class RefreshToken(
+    @Id
+    val accountId: String,
+
+    val token: String
+)
