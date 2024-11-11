c94abf574b4e1b7e5ca144ff50b44056cb14bae7
도훈 <modooboiroo@gmail.com>
2024-11-11 21:44:13 +0900

    feat :: JwtProperties

---
+++ src/main/kotlin/org/example/firstkopring/global/security/jwt/JwtProperties.kt
@@ -0,0 +1,15 @@
+package org.example.firstkopring.global.security.jwt
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties(
+    val header: String,
+    val prefix: String,
+    val secretKey: String,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val accessExpiration: Long,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val refreshExpiration: Long
+)
