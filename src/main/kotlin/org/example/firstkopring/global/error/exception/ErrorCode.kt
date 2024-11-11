1bcad90cb8278afc6c10b97af1e6c6ed8e3d3e99
도훈 <modooboiroo@gmail.com>
2024-11-11 22:52:49 +0900

    feat :: add INVALID_TOKEN Exception to ErrorCode

--- src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
@@ -4,6 +4,7 @@ enum class ErrorCode(
     val statusCode: Int,
     val message: String,
 ) {
+    INVALID_TOKEN(401, "invalid token"),
     BOARD_NOT_FOUND(404, "board not found."),
     USER_NOT_FOUND(404, "user not found"),
     INTERNAL_SERVER_ERROR(500, "Internal server error")
