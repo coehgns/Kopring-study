aad6a1cfc25d2182691ea42bad68ed43277cc6a6
도훈 <modooboiroo@gmail.com>
2024-11-12 08:12:01 +0900

    feat :: add USER_ALREADY_EXISTS Exception to ErrorCode

--- src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
@@ -7,5 +7,6 @@ enum class ErrorCode(
     INVALID_TOKEN(401, "invalid token"),
     BOARD_NOT_FOUND(404, "board not found."),
     USER_NOT_FOUND(404, "user not found"),
+    USER_ALREADY_EXISTS(409, "User Already Exists"),
     INTERNAL_SERVER_ERROR(500, "Internal server error")
 }
