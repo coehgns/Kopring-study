d090ec46285cf5a936e74aed1b4ca04bef012000
도훈 <modooboiroo@gmail.com>
2024-11-15 22:18:25 +0900

    feat :: add token validation to JwtFilter

--- src/main/kotlin/org/example/firstkopring/global/security/jwt/JwtFilter.kt
+++ src/main/kotlin/org/example/firstkopring/global/security/jwt/JwtFilter.kt
@@ -3,17 +3,29 @@ package org.example.firstkopring.global.security.jwt
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
 class JwtFilter(
-    private val jwtProvider: JwtProvider
+    private val jwtUtils: JwtUtils
 ): OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val token: String = jwtProvider.resolveToken(request).toString()
+        val authorizationHeader: String? = request.getHeader("Authorization")
+            ?: return filterChain.doFilter(request, response)
+
+        val token = authorizationHeader?.substring("Bearer ".length)
+            ?: return filterChain.doFilter(request, response)
+
+        if (jwtUtils.validation(token)) {
+            val username = jwtUtils.parseUsername(token)
+            val authentication: Authentication = jwtUtils.getAuthentication(username)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
 
         filterChain.doFilter(request, response)
     }
