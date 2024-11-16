16a71a3362f3045b5a8b453b65ef06ebdd53ae33
도훈 <modooboiroo@gmail.com>
2024-11-16 21:48:33 +0900

    feat :: add Board author mismatch exception to DeleteBoardService

--- src/main/kotlin/org/example/firstkopring/domain/board/sevice/DeleteBoardService.kt
+++ src/main/kotlin/org/example/firstkopring/domain/board/sevice/DeleteBoardService.kt
@@ -1,11 +1,28 @@
 package org.example.firstkopring.domain.board.sevice
 
+import org.example.firstkopring.domain.board.domain.Board
 import org.example.firstkopring.domain.board.domain.repository.BoardRepository
+import org.example.firstkopring.domain.user.facade.UserFacade
+import org.example.firstkopring.global.error.exception.BusinessException
+import org.example.firstkopring.global.error.exception.ErrorCode
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class DeleteBoardService(
-    private val boardRepository: BoardRepository
+    private val boardRepository: BoardRepository,
+    private val userFacade: UserFacade
 ) {
-    fun execute(id: Long) = boardRepository.deleteById(id)
+    fun execute(id: Long) {
+        val board: Board = boardRepository.findByIdOrNull(id)
+            ?: throw BusinessException(ErrorCode.BOARD_NOT_FOUND)
+
+        val currentUser = userFacade.currentUser()
+
+        if(currentUser != board.user) {
+            throw BusinessException(ErrorCode.BOARD_AUTHOR_MISMATCH)
+        }
+
+        boardRepository.deleteById(id)
+    }
 }
