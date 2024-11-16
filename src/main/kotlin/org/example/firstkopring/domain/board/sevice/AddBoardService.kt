6f96f6ab9bde4a74088becaf3a842c2745d96dc7
도훈 <modooboiroo@gmail.com>
2024-11-16 21:51:10 +0900

    feat :: add current user save logic to AddBoardService

--- src/main/kotlin/org/example/firstkopring/domain/board/sevice/AddBoardService.kt
+++ src/main/kotlin/org/example/firstkopring/domain/board/sevice/AddBoardService.kt
@@ -3,18 +3,24 @@ package org.example.firstkopring.domain.board.sevice
 import org.example.firstkopring.domain.board.domain.Board
 import org.example.firstkopring.domain.board.domain.repository.BoardRepository
 import org.example.firstkopring.domain.board.presentation.dto.request.BoardRequest
+import org.example.firstkopring.domain.user.domain.User
+import org.example.firstkopring.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
 
 @Service
 class AddBoardService(
-    private val boardRepository: BoardRepository
+    private val boardRepository: BoardRepository,
+    private val userFacade: UserFacade
 ) {
     fun execute(boardRequest: BoardRequest) {
+        val currentUser: User = userFacade.currentUser()
+
         boardRepository.save(
             boardRequest.run {
                 Board(
                     title = title,
-                    content = content
+                    content = content,
+                    user = currentUser
                 )
             }
         )
