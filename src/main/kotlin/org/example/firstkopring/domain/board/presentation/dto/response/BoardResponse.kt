74000197f920325b91d282bcdc2f0b7208eaa08d
도훈 <modooboiroo@gmail.com>
2024-11-16 22:00:26 +0900

    feat :: add BoardResponse static factory method to BoardResponse

--- src/main/kotlin/org/example/firstkopring/domain/board/presentation/dto/response/BoardResponse.kt
+++ src/main/kotlin/org/example/firstkopring/domain/board/presentation/dto/response/BoardResponse.kt
@@ -1,7 +1,20 @@
 package org.example.firstkopring.domain.board.presentation.dto.response
 
+import org.example.firstkopring.domain.board.domain.Board
+import org.example.firstkopring.domain.user.domain.User
+
 data class BoardResponse(
-    val user: String,
+    val user: User?,
     val title: String,
     val content: String
-)
+) {
+    companion object {
+        fun BoardResponse(board: Board): BoardResponse {
+            return BoardResponse(
+                user = board.user,
+                title = board.title,
+                content = board.content
+            )
+        }
+    }
+}
