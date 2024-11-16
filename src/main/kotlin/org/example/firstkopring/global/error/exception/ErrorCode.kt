485c3cd5b898486278da08276af553d590242000
도훈 <modooboiroo@gmail.com>
2024-11-16 21:40:34 +0900

    feat :: add Board missmatch exception to ErrorCode

--- src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
@@ -6,6 +6,7 @@ enum class ErrorCode(
 ) {
     INVALID_TOKEN(401, "invalid token"),
     PASSWORD_MISMATCH(401, "password mismatch"),
+    BOARD_AUTHOR_MISMATCH(403, "Board author mismatch"),
     BOARD_NOT_FOUND(404, "board not found."),
     USER_NOT_FOUND(404, "user not found"),
     USER_ALREADY_EXISTS(409, "User Already Exists"),
