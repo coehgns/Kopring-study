ca96000c368b4a6c4a1f1180e17e9e557406fd3e
도훈 <modooboiroo@gmail.com>
2024-11-16 21:41:00 +0900

    feat :: add Board missmatch exception to ModifyBoardService

--- src/main/kotlin/org/example/firstkopring/domain/board/sevice/ModifyBoardService.kt
+++ src/main/kotlin/org/example/firstkopring/domain/board/sevice/ModifyBoardService.kt
@@ -2,6 +2,7 @@ package org.example.firstkopring.domain.board.sevice
 
 import org.example.firstkopring.domain.board.domain.repository.BoardRepository
 import org.example.firstkopring.domain.board.presentation.dto.request.ModifyBoardRequest
+import org.example.firstkopring.domain.user.facade.UserFacade
 import org.example.firstkopring.global.error.exception.BusinessException
 import org.example.firstkopring.global.error.exception.ErrorCode
 import org.springframework.data.repository.findByIdOrNull
@@ -9,11 +10,18 @@ import org.springframework.stereotype.Service
 
 @Service
 class ModifyBoardService(
-    private val boardRepository: BoardRepository
+    private val boardRepository: BoardRepository,
+    private val userFacade: UserFacade
 ) {
     fun execute(id: Long, modifyBoardRequest: ModifyBoardRequest) {
         val board = boardRepository.findByIdOrNull(id)
             ?: throw BusinessException(ErrorCode.BOARD_NOT_FOUND)
+
+        val currentUser = userFacade.currentUser()
+
+        if(currentUser == board.user) {
+            throw BusinessException(ErrorCode.BOARD_AUTHOR_MISMATCH)
+        }
 
         board.modifyBoard(modifyBoardRequest.title, modifyBoardRequest.content)
         boardRepository.save(board)
