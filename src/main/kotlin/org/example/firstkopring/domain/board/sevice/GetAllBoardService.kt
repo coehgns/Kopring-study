56b78e920abc602b884f144ed5ba54943b4a9114
도훈 <modooboiroo@gmail.com>
2024-11-16 22:06:55 +0900

    refactor :: modify return type of get all board method to GetAllBoardService

--- src/main/kotlin/org/example/firstkopring/domain/board/sevice/GetAllBoardService.kt
+++ src/main/kotlin/org/example/firstkopring/domain/board/sevice/GetAllBoardService.kt
@@ -1,12 +1,18 @@
 package org.example.firstkopring.domain.board.sevice
 
-import org.example.firstkopring.domain.board.domain.Board
 import org.example.firstkopring.domain.board.domain.repository.BoardRepository
+import org.example.firstkopring.domain.board.presentation.dto.response.BoardResponse
 import org.springframework.stereotype.Service
+import java.util.stream.Collectors
 
 @Service
 class GetAllBoardService(
     private val boardRepository: BoardRepository
 ) {
-    fun execute() : List<Board> = boardRepository.findAll()
+    fun execute() : List<BoardResponse> {
+        return boardRepository.findAll()
+            .stream()
+            .map(BoardResponse::fromBoard)
+            .collect(Collectors.toList())
+    }
 }
