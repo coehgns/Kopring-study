561a4e77a72b9f493892b5760db4c3a0e4d6a033
도훈 <modooboiroo@gmail.com>
2024-11-16 22:03:09 +0900

    refactor :: return type of get board method to GetBoardService

--- src/main/kotlin/org/example/firstkopring/domain/board/sevice/GetBoardService.kt
+++ src/main/kotlin/org/example/firstkopring/domain/board/sevice/GetBoardService.kt
@@ -2,17 +2,20 @@ package org.example.firstkopring.domain.board.sevice
 
 import org.example.firstkopring.domain.board.domain.Board
 import org.example.firstkopring.domain.board.domain.repository.BoardRepository
+import org.example.firstkopring.domain.board.presentation.dto.response.BoardResponse
 import org.example.firstkopring.global.error.exception.BusinessException
 import org.example.firstkopring.global.error.exception.ErrorCode
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 
 @Service
 class GetBoardService(
     private val boardRepository: BoardRepository
 ) {
-    fun execute(id: Long) : Board =
-        boardRepository.findByIdOrNull(id)
+    fun execute(id: Long): BoardResponse {
+        val board: Board = boardRepository.findByIdOrNull(id)
             ?: throw BusinessException(ErrorCode.BOARD_NOT_FOUND)
+
+        return BoardResponse.fromBoard(board)
+    }
 }
