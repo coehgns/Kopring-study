ca56538f24bf8127d7b1a435b11613e18de5a343
도훈 <modooboiroo@gmail.com>
2024-12-15 23:37:32 +0900

    feat :: JwtProvider

---
+++ src/main/kotlin/org/example/firstkopring/global/security/jwt/JwtProvider.kt
@@ -0,0 +1,13 @@
+package org.example.firstkopring.global.security.jwt
+
+import org.example.firstkopring.domain.auth.domain.repository.RefreshTokenRepository
+import org.example.firstkopring.global.security.auth.CustomUserDetailsService
+import org.springframework.stereotype.Component
+
+@Component
+class JwtProvider(
+    private val refreshTokenRepository: RefreshTokenRepository,
+    private val customUserDetailsService: CustomUserDetailsService,
+    private val jwtProperties: JwtProperties
+) {
+}
