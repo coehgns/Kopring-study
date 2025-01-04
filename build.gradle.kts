1f5ed6e16cee9e7e5bc18e0f9a1590ef5615ebfd
도훈 <modooboiroo@gmail.com>
2025-01-04 21:23:10 +0900

    feat :: add redis setting to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
