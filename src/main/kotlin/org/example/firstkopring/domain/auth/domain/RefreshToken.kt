96539606be7611b6005c4b10d98c339d0b135165
도훈 <modooboiroo@gmail.com>
2025-01-04 21:25:21 +0900

    feat :: add redis setting to RefreshToken

--- src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
@@ -2,7 +2,10 @@ package org.example.firstkopring.domain.auth.domain
 
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
 
+@RedisHash
 @Table(name = "refresh_token")
 class RefreshToken(
     @Id
@@ -10,5 +13,6 @@ class RefreshToken(
 
     val token: String,
 
+    @TimeToLive
     val expiration: Long
 )
