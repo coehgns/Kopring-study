d85ab1d9b3aad62d9f8f901099dba6b81a87c1a9
도훈 <modooboiroo@gmail.com>
2025-01-04 21:24:51 +0900

    feat :: remove @Entity annotation to RefreshToken

--- src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
@@ -1,10 +1,8 @@
 package org.example.firstkopring.domain.auth.domain
 
-import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 
-@Entity
 @Table(name = "refresh_token")
 class RefreshToken(
     @Id
