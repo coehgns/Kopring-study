3d782f89cdf127b3b505c142ba3d49b9d55bc2b6
도훈 <modooboiroo@gmail.com>
2025-01-04 21:28:53 +0900

    feat :: modify RefreshTokenRepository extends repository to RefreshTokenRepository

--- src/main/kotlin/org/example/firstkopring/domain/auth/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -1,10 +1,10 @@
 package org.example.firstkopring.domain.auth.domain.repository
 
 import org.example.firstkopring.domain.auth.domain.RefreshToken
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RefreshTokenRepository: JpaRepository<RefreshToken, Long> {
+interface RefreshTokenRepository: CrudRepository<RefreshToken, Long> {
     fun findByToken(token: String): RefreshToken
 }
