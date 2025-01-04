07b6307d17ef9f79294c685aa1c88f0adf73a241
도훈 <modooboiroo@gmail.com>
2025-01-04 21:26:30 +0900

    feat :: add @Column annotation to RefreshToken

--- src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
@@ -1,5 +1,6 @@
 package org.example.firstkopring.domain.auth.domain
 
+import jakarta.persistence.Column
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.springframework.data.redis.core.RedisHash
@@ -9,10 +10,13 @@ import org.springframework.data.redis.core.TimeToLive
 @Table(name = "refresh_token")
 class RefreshToken(
     @Id
+    @Column(name = "id", nullable = false, unique = true)
     val accountId: String,
 
+    @Column(name = "refresh_token", nullable = false)
     val token: String,
 
     @TimeToLive
+    @Column(name = "token_expiration", nullable = false)
     val expiration: Long
 )
