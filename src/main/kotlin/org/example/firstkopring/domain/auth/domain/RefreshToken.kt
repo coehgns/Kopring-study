c3fce06a51608c4a3c4cd7f13502bd41ec02482c
도훈 <modooboiroo@gmail.com>
2025-01-04 21:24:38 +0900

    feat :: add @Table annotation to RefreshToken

--- src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/domain/RefreshToken.kt
@@ -2,8 +2,10 @@ package org.example.firstkopring.domain.auth.domain
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "refresh_token")
 class RefreshToken(
     @Id
     val accountId: String,
