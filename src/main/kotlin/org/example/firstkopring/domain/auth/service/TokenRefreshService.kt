984f1eb2ea69371600d1f685577cea729e2366ab
도훈 <modooboiroo@gmail.com>
2025-01-05 22:57:24 +0900

    feat :: add request: ReissueRequest to TokenRefreshService

--- src/main/kotlin/org/example/firstkopring/domain/auth/service/TokenRefreshService.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/service/TokenRefreshService.kt
@@ -3,6 +3,7 @@ package org.example.firstkopring.domain.auth.service
 import org.example.firstkopring.domain.auth.domain.RefreshToken
 import org.example.firstkopring.domain.auth.domain.repository.RefreshTokenRepository
 import org.example.firstkopring.domain.auth.exception.RefreshTokenNotFoundException
+import org.example.firstkopring.domain.auth.presentation.dto.request.ReissueRequest
 import org.example.firstkopring.domain.auth.presentation.dto.response.TokenResponse
 import org.example.firstkopring.global.security.jwt.JwtProperties
 import org.example.firstkopring.global.security.jwt.JwtTokenProvider
@@ -14,8 +15,8 @@ class TokenRefreshService(
     private val jwtTokenProvider: JwtTokenProvider,
     private val jwtProperties: JwtProperties
 ) {
-    fun execute(rfToken: String): TokenResponse {
-        val refreshToken: RefreshToken = refreshTokenRepository.findByToken(rfToken)
+    fun execute(request: ReissueRequest): TokenResponse {
+        val refreshToken: RefreshToken = refreshTokenRepository.findByToken(request.refreshToken)
             ?: throw RefreshTokenNotFoundException
 
         val tokens = jwtTokenProvider.generateToken(refreshToken.username)
