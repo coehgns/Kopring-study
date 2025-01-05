5a920eb2ff7a5d2d95487968520069fe4d55a08e
도훈 <modooboiroo@gmail.com>
2025-01-05 22:48:41 +0900

    feat :: RefreshTokenNotFoundException

---
+++ src/main/kotlin/org/example/firstkopring/domain/auth/exception/RefreshTokenNotFoundException.kt
@@ -0,0 +1,6 @@
+package org.example.firstkopring.domain.auth.exception
+
+import org.example.firstkopring.global.error.exception.BusinessException
+import org.example.firstkopring.global.error.exception.ErrorCode
+
+object RefreshTokenNotFoundException: BusinessException(ErrorCode.RefreshTokenNotFoundException)
