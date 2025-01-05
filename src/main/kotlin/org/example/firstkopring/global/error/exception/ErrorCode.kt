b7d517005ae9b93d30c53e12529da90bcdaf9f87
도훈 <modooboiroo@gmail.com>
2025-01-05 22:48:08 +0900

    feat :: RefreshTokenNotFoundException

--- src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/org/example/firstkopring/global/error/exception/ErrorCode.kt
@@ -10,6 +10,7 @@ enum class ErrorCode(
     BOARD_AUTHOR_MISMATCH(403, "Board author mismatch"),
     BOARD_NOT_FOUND(404, "board not found."),
     USER_NOT_FOUND(404, "user not found"),
+    RefreshTokenNotFoundException(404, "refreshToken not found."),
     USER_ALREADY_EXISTS(409, "User Already Exists"),
     INTERNAL_SERVER_ERROR(500, "Internal server error")
 }
