e4876e3179c36304df08c007e388891abdf969a2
도훈 <modooboiroo@gmail.com>
2025-01-05 22:28:51 +0900

    feat :: AuthController

---
+++ src/main/kotlin/org/example/firstkopring/domain/auth/presentation/AuthController.kt
@@ -0,0 +1,23 @@
+package org.example.firstkopring.domain.auth.presentation
+
+import org.example.firstkopring.domain.auth.presentation.dto.request.SignupRequest
+import org.example.firstkopring.domain.auth.presentation.dto.response.TokenResponse
+import org.example.firstkopring.domain.auth.service.SignupService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/auth")
+class AuthController(
+    private val signupService: SignupService
+) {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    fun signup(@RequestBody request: SignupRequest): TokenResponse {
+        return signupService.execute(request)
+    }
+}
