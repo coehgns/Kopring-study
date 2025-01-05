c85be1dc1dd8a319c9490a932742c33cd9e5af1e
도훈 <modooboiroo@gmail.com>
2025-01-05 22:15:52 +0900

    refactor :: remove private to TokenResponse

--- src/main/kotlin/org/example/firstkopring/domain/auth/presentation/dto/response/TokenResponse.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/presentation/dto/response/TokenResponse.kt
@@ -3,8 +3,8 @@ package org.example.firstkopring.domain.auth.presentation.dto.response
 import java.util.Date
 
 data class TokenResponse(
-    private val accessToken: String,
-    private val refreshToken: String,
-    private val accessExp: Date,
-    private val refreshExp: Date
+    val accessToken: String,
+    val refreshToken: String,
+    val accessExp: Date,
+    val refreshExp: Date
 )
