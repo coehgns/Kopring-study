33d2621dd83c50307729b9a37b82883d3c977ed3
도훈 <modooboiroo@gmail.com>
2025-01-05 22:45:39 +0900

    feat :: add login method to AuthController

--- src/main/kotlin/org/example/firstkopring/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/presentation/AuthController.kt
@@ -1,7 +1,9 @@
 package org.example.firstkopring.domain.auth.presentation
 
+import org.example.firstkopring.domain.auth.presentation.dto.request.LoginRequest
 import org.example.firstkopring.domain.auth.presentation.dto.request.SignupRequest
 import org.example.firstkopring.domain.auth.presentation.dto.response.TokenResponse
+import org.example.firstkopring.domain.auth.service.LoginService
 import org.example.firstkopring.domain.auth.service.SignupService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/auth")
 class AuthController(
-    private val signupService: SignupService
+    private val signupService: SignupService,
+    private val loginService: LoginService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
+    @PostMapping("/signup")
     fun signup(@RequestBody request: SignupRequest): TokenResponse {
         return signupService.execute(request)
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody request: LoginRequest): TokenResponse {
+        return loginService.execute(request)
     }
 }
