5e03809b74bf203db5afdbd907934a459d2d04d6
도훈 <modooboiroo@gmail.com>
2025-01-05 22:17:34 +0900

    refactor :: modify the type of accessExp to TokenResponse

--- src/main/kotlin/org/example/firstkopring/domain/auth/presentation/dto/response/TokenResponse.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/presentation/dto/response/TokenResponse.kt
@@ -1,9 +1,9 @@
 package org.example.firstkopring.domain.auth.presentation.dto.response
 
-import java.util.Date
+import java.time.LocalDateTime
 
 data class TokenResponse(
     val accessToken: String,
+    val accessExp: LocalDateTime,
     val refreshToken: String,
-    val accessExp: Date,
 )
