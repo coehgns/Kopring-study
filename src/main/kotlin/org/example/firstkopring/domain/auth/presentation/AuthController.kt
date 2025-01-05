62b8d3d3852b1ac1235d879650bc969938723336
도훈 <modooboiroo@gmail.com>
2025-01-05 22:57:37 +0900

    feat :: add reissue method to AuthController

--- src/main/kotlin/org/example/firstkopring/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/org/example/firstkopring/domain/auth/presentation/AuthController.kt
@@ -1,10 +1,12 @@
 package org.example.firstkopring.domain.auth.presentation
 
 import org.example.firstkopring.domain.auth.presentation.dto.request.LoginRequest
+import org.example.firstkopring.domain.auth.presentation.dto.request.ReissueRequest
 import org.example.firstkopring.domain.auth.presentation.dto.request.SignupRequest
 import org.example.firstkopring.domain.auth.presentation.dto.response.TokenResponse
 import org.example.firstkopring.domain.auth.service.LoginService
 import org.example.firstkopring.domain.auth.service.SignupService
+import org.example.firstkopring.domain.auth.service.TokenRefreshService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/auth")
 class AuthController(
     private val signupService: SignupService,
-    private val loginService: LoginService
+    private val loginService: LoginService,
+    private val tokenRefreshService: TokenRefreshService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
@@ -27,5 +30,10 @@ class AuthController(
     @PostMapping("/login")
     fun login(@RequestBody request: LoginRequest): TokenResponse {
         return loginService.execute(request)
+    }
+
+    @PostMapping("re-issue")
+    fun reissue(@RequestBody request: ReissueRequest): TokenResponse {
+        return tokenRefreshService.execute(request)
     }
 }
