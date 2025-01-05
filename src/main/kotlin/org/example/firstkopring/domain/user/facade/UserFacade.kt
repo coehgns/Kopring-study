0c796e5305c415c84709dd103e63c5ac96f0e917
도훈 <modooboiroo@gmail.com>
2025-01-05 22:35:23 +0900

    feat :: add checkPassword method to UserFacade

--- src/main/kotlin/org/example/firstkopring/domain/user/facade/UserFacade.kt
+++ src/main/kotlin/org/example/firstkopring/domain/user/facade/UserFacade.kt
@@ -1,16 +1,19 @@
 package org.example.firstkopring.domain.user.facade
 
+import org.example.firstkopring.domain.auth.exception.PasswordMissmatchException
 import org.example.firstkopring.domain.user.domain.User
 import org.example.firstkopring.domain.user.domain.repository.UserRepository
 import org.example.firstkopring.domain.user.exception.UserAlreadyExistException
 import org.example.firstkopring.global.error.exception.BusinessException
 import org.example.firstkopring.global.error.exception.ErrorCode
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
 class UserFacade(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
     fun currentUser(): User {
         val username: String = SecurityContextHolder.getContext().authentication.name
@@ -21,5 +24,14 @@ class UserFacade(
     fun checkUserExist(username: String) {
         userRepository.findByUsername(username)
             ?: throw UserAlreadyExistException
+    }
+
+    fun checkPassword(
+        user: User,
+        password: String
+    ) {
+        if (passwordEncoder.matches(user.password, password)) {
+            throw PasswordMissmatchException
+        }
     }
 }
