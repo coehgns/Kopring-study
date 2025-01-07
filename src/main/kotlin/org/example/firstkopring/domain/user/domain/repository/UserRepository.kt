ffa9a55907893e1ac2007b6f80193f1a0aed2f18
도훈 <modooboiroo@gmail.com>
2025-01-07 21:47:39 +0900

    refactor :: remove unnecessary method to UserRepository

--- src/main/kotlin/org/example/firstkopring/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/org/example/firstkopring/domain/user/domain/repository/UserRepository.kt
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun findByUsername(username: String): User?
-    fun existsByUsername(username: String): Boolean
 }
