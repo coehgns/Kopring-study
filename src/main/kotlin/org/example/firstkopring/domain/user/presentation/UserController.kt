3b08acbcbb51e3948abb3c6247bccae25359347e
도훈 <modooboiroo@gmail.com>
2025-01-13 09:24:38 +0900

    feat :: add getUserInfo method to UserController

--- src/main/kotlin/org/example/firstkopring/domain/user/presentation/UserController.kt
+++ src/main/kotlin/org/example/firstkopring/domain/user/presentation/UserController.kt
@@ -1,18 +1,27 @@
 package org.example.firstkopring.domain.user.presentation
 
 import org.example.firstkopring.domain.user.presentation.dto.response.GetMyInfoResponse
+import org.example.firstkopring.domain.user.presentation.dto.response.GetUserInfoResponse
 import org.example.firstkopring.domain.user.service.GetMyInfoService
+import org.example.firstkopring.domain.user.service.GetUserInfoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/user")
 class UserController(
-    private val getMyInfoService: GetMyInfoService
+    private val getMyInfoService: GetMyInfoService,
+    private val getUserInfoService: GetUserInfoService
 ) {
     @GetMapping
     fun getMyInfo(): GetMyInfoResponse {
         return getMyInfoService.execute()
+    }
+
+    @GetMapping("/{user-id}")
+    fun getUserInfo(@PathVariable("user-id") userid: Long): GetUserInfoResponse {
+        return getUserInfoService.execute(userid)
     }
 }
