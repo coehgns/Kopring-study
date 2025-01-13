9d2b1524331392070bb44aef2cedd810c392950e
도훈 <modooboiroo@gmail.com>
2025-01-13 09:31:53 +0900

    feat :: add changePassword method to UserController

--- src/main/kotlin/org/example/firstkopring/domain/user/presentation/UserController.kt
+++ src/main/kotlin/org/example/firstkopring/domain/user/presentation/UserController.kt
@@ -1,11 +1,15 @@
 package org.example.firstkopring.domain.user.presentation
 
+import org.example.firstkopring.domain.user.presentation.dto.request.ChangePasswordRequest
 import org.example.firstkopring.domain.user.presentation.dto.response.GetMyInfoResponse
 import org.example.firstkopring.domain.user.presentation.dto.response.GetUserInfoResponse
+import org.example.firstkopring.domain.user.service.ChangePasswordService
 import org.example.firstkopring.domain.user.service.GetMyInfoService
 import org.example.firstkopring.domain.user.service.GetUserInfoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/users")
 class UserController(
     private val getMyInfoService: GetMyInfoService,
-    private val getUserInfoService: GetUserInfoService
+    private val getUserInfoService: GetUserInfoService,
+    private val changePasswordService: ChangePasswordService
 ) {
     @GetMapping
     fun getMyInfo(): GetMyInfoResponse {
@@ -23,5 +28,10 @@ class UserController(
     @GetMapping("/{user-id}")
     fun getUserInfo(@PathVariable("user-id") userid: Long): GetUserInfoResponse {
         return getUserInfoService.execute(userid)
+    }
+
+    @PatchMapping("/password")
+    fun changePassword(@RequestBody request: ChangePasswordRequest) {
+        changePasswordService.execute(request)
     }
 }
